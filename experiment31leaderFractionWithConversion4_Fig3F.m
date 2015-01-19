1d8423db1dd8f7ad810094684e65475032f8993f
Linus Schumacher <schumacher@maths.ox.ac.uk>
2015-01-19 15:35:43 +0000

    Updated figures and comments in VEGF manuscript draft. -- LJS

---
+++ experiment31leaderFractionWithConversion4_Fig3F.m
@@ -0,0 +1,68 @@
+% plot various migration statistics vs switching time lead2follow
+% and follow2lead
+% L.J. Schumacher 22.10.14
+
+close all
+clear
+
+precision = 2; % significant figures for filenames and plot labels etc.
+
+sensingAccuracyValues = [0.1, 0.01];
+
+% load variables from saved collated results
+load('manuscripts/VEGF/figures/experiment31conv4collatedResults.mat','xBins','cellDistributions','actualLeaderFraction','lead2followValues','follow2leadValues','numCells','referenceCellDistribution','referenceLeaderFraction','referenceNumCells')
+%% check that we run enough repeats of simulations
+% plotMuSigmaErr(1:20,squeeze(numCells(2,2,1,:))','<n>',10)
+
+%% plot the results
+% for contour plots, take to take the transpose if data is (x,y), to ensure that
+% columns are plotted at x-values and rows at y-values (after transpose)
+
+[L2F,F2L] = meshgrid(lead2followValues,follow2leadValues); % for plotting 'patterns' inside contours
+nLevels = 20;
+axisticks = [1 8 16 24 32 40 48 56];
+defaultFollow = [2]
+sensAccCtr = 1;
+
+migrationEfficiency = squeeze(mean(numCells(defaultFollow + 1,sensAccCtr,:,:,:),5))/mean(referenceNumCells);
+migrationCOV = squeeze(std(numCells(defaultFollow + 1,sensAccCtr,:,:,:),0,5)...
+    ./mean(numCells(defaultFollow + 1,sensAccCtr,:,:,:),5));
+contourf(lead2followValues,follow2leadValues,migrationEfficiency',linspace(0,1,nLevels+1),...
+    'EdgeColor','none')
+% pcolor(lead2followValues,follow2leadValues,migrationEfficiency)
+% shading interp
+cb = colorbar; cb.Label.String = 'relative migration efficiency';
+caxis([0 1]), colormap(parula(nLevels))
+xlabel('lead -> follow (min)'), ylabel('follow -> lead (min)')
+set(gca,'xtick',axisticks,'ytick',axisticks)
+% plot contours showing coefficient of variation
+hold on
+contour(lead2followValues,follow2leadValues,migrationCOV',[0.15 0.15],...
+    'Color',[1 1 1], 'LineWidth', 1);
+contourIdcs = migrationCOV'>=0.15&migrationCOV'<0.2;
+scatter(L2F(contourIdcs),F2L(contourIdcs),'.','MarkerEdgeColor',[1 1 1])
+%         [~, covBins] = hist(migrationCOV(:),20);
+%         contour(lead2followValues,follow2leadValues,migrationCOV',covBins(covBins>=0.15&covBins<0.2),...
+%             'Color',[1 1 1], 'LineStyle', ':')
+contour(lead2followValues,follow2leadValues,migrationCOV',[0.2 0.2],...
+    'Color',[1 1 1]/2, 'LineWidth', 1)
+contourIdcs = migrationCOV'>0.2;
+scatter(L2F(contourIdcs),F2L(contourIdcs),'.','MarkerEdgeColor',[1 1 1]/2)
+%         contour(lead2followValues,follow2leadValues,migrationCOV',covBins(covBins>=0.2),...
+%             'Color',[1 1 1]*0.5, 'LineStyle', ':')
+%         contour(lead2followValues,follow2leadValues,migrationCOV',[0.25 0.25],...
+%             'Color',[1 1 1]*0, 'LineWidth', 1)
+
+%% export figure
+exportOptions = struct('Format','eps2',...
+    'Width','18.0',...
+    'Color','rgb',...
+    'Resolution',300,...
+    'FontMode','fixed',...
+    'FontSize',10,...
+    'LineWidth',2);
+
+filename = 'manuscripts/VEGF/figures/Fig3F';
+set(gcf,'PaperUnits','centimeters');
+exportfig(gcf,[filename '.eps'],exportOptions);
+system(['epstopdf ' filename '.eps']);
