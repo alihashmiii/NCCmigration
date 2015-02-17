4a70dd294365e78d13446f42b04fb70c87d23a10
Linus Schumacher <schumacher@maths.ox.ac.uk>
2015-02-17 13:14:12 +0000

    One updated and one new figure panel for the second manuscript we are preparing for submission. They are for Fig.3 (I&S experiment and effect on model migration). The new panel highlights better that the migration outcome and its variability depend on the ratio of switching times. -- LJS

--- experiment31leaderFractionWithConversion4_Fig3F.m
+++ experiment31leaderFractionWithConversion4_Fig3F.m
@@ -6,6 +6,8 @@ close all
 clear
 
 precision = 2; % significant figures for filenames and plot labels etc.
+numReps = 20;
+plotError = 0;
 
 sensingAccuracyValues = [0.1, 0.01];
 
@@ -20,9 +22,9 @@ load('manuscripts/VEGF/figures/experiment31conv4collatedResults.mat','xBins','ce
 [L2F,F2L] = meshgrid(lead2followValues,follow2leadValues); % for plotting 'patterns' inside contours
 nLevels = 20;
 axisticks = [1 8 16 24 32 40 48 56];
-defaultFollow = [2]
+defaultFollow = [2];
 sensAccCtr = 1;
-
+contourFig = figure;
 migrationEfficiency = squeeze(mean(numCells(defaultFollow + 1,sensAccCtr,:,:,:),5))/mean(referenceNumCells);
 migrationCOV = squeeze(std(numCells(defaultFollow + 1,sensAccCtr,:,:,:),0,5)...
     ./mean(numCells(defaultFollow + 1,sensAccCtr,:,:,:),5));
@@ -32,15 +34,15 @@ contourf(lead2followValues,follow2leadValues,migrationEfficiency',linspace(0,1,n
 % shading interp
 cb = colorbar; cb.Label.String = 'relative migration efficiency';
 caxis([0 1]), colormap(parula(nLevels))
-xlabel('lead -> follow (min)'), ylabel('follow -> lead (min)')
+xlabel('switch time lead -> follow, \tau_{L->F} (min)')
+ylabel('switch time follow -> lead, \tau_{F->L} (min)')
 set(gca,'xtick',axisticks,'ytick',axisticks)
 % plot contours showing coefficient of variation
 hold on
-contour(lead2followValues,follow2leadValues,migrationCOV',[0.15 0.15],...
-    'Color',[1 1 1], 'LineWidth', 1);
+contour(lead2followValues,follow2leadValues,migrationCOV',[0.15 0.15],'Color',[1 1 1]);
 contourIdcs = migrationCOV'>=0.15&migrationCOV'<0.2;
 scatter(L2F(contourIdcs),F2L(contourIdcs),'.','MarkerEdgeColor',[1 1 1])
-%         [~, covBins] = hist(migrationCOV(:),20);
+%         [~, covBins] = hist(migrationCOV(:),25);
 %         contour(lead2followValues,follow2leadValues,migrationCOV',covBins(covBins>=0.15&covBins<0.2),...
 %             'Color',[1 1 1], 'LineStyle', ':')
 contour(lead2followValues,follow2leadValues,migrationCOV',[0.2 0.2],...
@@ -51,10 +53,34 @@ scatter(L2F(contourIdcs),F2L(contourIdcs),'.','MarkerEdgeColor',[1 1 1]/2)
 %             'Color',[1 1 1]*0.5, 'LineStyle', ':')
 %         contour(lead2followValues,follow2leadValues,migrationCOV',[0.25 0.25],...
 %             'Color',[1 1 1]*0, 'LineWidth', 1)
-
+%%
+ratioFig = figure;
+hold on
+plotHandles = NaN(size(lead2followValues'));
+for tauCtr = 1:length(lead2followValues)
+    if plotError
+        plotHandles(tauCtr) = errorbar(lead2followValues./follow2leadValues(tauCtr),migrationEfficiency(tauCtr,:),...
+            migrationCOV(tauCtr,:)./sqrt(numReps));
+    else
+        plotHandles(tauCtr) = plot(lead2followValues./follow2leadValues(tauCtr),migrationEfficiency(tauCtr,:));
+        plot(lead2followValues./follow2leadValues(tauCtr),migrationCOV(tauCtr,:),...
+            '--','Color',get(plotHandles(tauCtr),'Color'));
+    end
+end
+plot([1 1],[0 1],'k:')
+xlim([0 20])
+xlabel('relative switch time \tau_{L->F}/\tau_{F->L}')
+if plotError
+    ylabel('migration efficiency, \mu \pm \mu/\sigma')
+else
+    ylabel('migration efficiency, \mu (-), \mu/\sigma (--)')
+end
+legendHandle = legend(plotHandles,[num2str(follow2leadValues'), ...
+    repmat(' min',length(follow2leadValues),1)]);
+text(15,1,'\tau_{F->L}')
 %% export figure
 exportOptions = struct('Format','eps2',...
-    'Width','18.0',...
+    'Width','10.0',...
     'Color','rgb',...
     'Resolution',300,...
     'FontMode','fixed',...
@@ -62,6 +88,11 @@ exportOptions = struct('Format','eps2',...
     'LineWidth',2);
 
 filename = 'manuscripts/VEGF/figures/Fig3F';
-set(gcf,'PaperUnits','centimeters');
-exportfig(gcf,[filename '.eps'],exportOptions);
+set(contourFig,'PaperUnits','centimeters');
+exportfig(contourFig,[filename '.eps'],exportOptions);
+system(['epstopdf ' filename '.eps']);
+
+filename = 'manuscripts/VEGF/figures/Fig3G';
+set(ratioFig,'PaperUnits','centimeters');
+exportfig(ratioFig,[filename '.eps'],exportOptions);
 system(['epstopdf ' filename '.eps']);
