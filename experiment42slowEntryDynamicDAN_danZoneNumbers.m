07e93af490e50415f94deb5a0c057e61776c0856
Linus Schumacher <linus@Linuss-MacBook-Pro.local>
2018-10-15 13:57:40 +0100

    commit before checking compatibility with matlab R2018a

--- experiment42slowEntryDynamicDAN_danZoneNumbers.m
+++ experiment42slowEntryDynamicDAN_danZoneNumbers.m
@@ -61,9 +61,11 @@ for cntGdn = {'parallel'}
                     % load cell positions into variables
                     cells = out.cells_save{timeIdx}; % all cells
                     % calculate cell fractions in/out of DAN zone vs time
-                    refCellFractions(insertEveryStepsCtr,repCtr) = nnz(cells(1,:)<=max(out.xlat_save{timeIdx})/3)/size(cells,2);
+                    refCellFractions(insertEveryStepsCtr,repCtr) = ...
+                        nnz(cells(1,:)<=max(out.xlat_save{timeIdx})/3)/size(cells,2);
                     % calc stream width
-                    refStreamWidths(insertEveryStepsCtr,repCtr) = std(cells(2,cells(1,:)<=max(out.xlat_save{timeIdx})/3));
+                    refStreamWidths(insertEveryStepsCtr,repCtr) = ...
+                        std(cells(2,cells(1,:)<=max(out.xlat_save{timeIdx})/3));
                 end
                 %% load DAN data
                 for repCtr = 1:numReps
@@ -91,15 +93,17 @@ for cntGdn = {'parallel'}
             end
             %
 %             errorbar(danZoneFig.Children,60./insertEveryStepsValues,mean(refCellFractions,2),std(cellFractions,0,2)/sqrt(numReps))
-            errorbar(danZoneFig.Children,60./insertEveryStepsValues,mean(cellFractions,2),std(cellFractions,0,2)/sqrt(numReps))
+%             errorbar(danZoneFig.Children,60./insertEveryStepsValues,mean(cellFractions,2),std(cellFractions,0,2)/sqrt(numReps))
+            set(0,'CurrentFigure',danZoneFig)
+            notBoxPlot(cellFractions',60./insertEveryStepsValues)
             xlabel(danZoneFig.Children,'attempted cell insertions (per hour)')
             danZoneFig.Children.XDir = 'reverse';
             ylabel(danZoneFig.Children,'fraction of cells in DAN zone')
-            xlim(danZoneFig.Children,[1 10])
+%             xlim(danZoneFig.Children,[1 10])
             danZoneFig.Children.XGrid = 'on';
             danZoneFig.Children.YGrid = 'on';
             danZoneFig.Children.Box = 'on';
-            danZoneFig.Children.YLim = [0.82, 0.94];
+            danZoneFig.Children.YLim = [0.75, 1];
             danZoneFig.Children.YTickLabel = num2str(danZoneFig.Children.YTick','%1.2f');
 %             hl = legend(danZoneFig.Children,{'no DAN';['v_{DAN} = ' num2str(slowSpeed)]},...
 %                 'Location','NorthWest');
